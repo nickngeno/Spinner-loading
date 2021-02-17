df4ab3d271a41be7e914152a761ebb8240f1da1d
nicholas <nickngeno1@gmail.com>
2021-02-17 11:51:35 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Spinner from 'react-bootstrap/Spinner'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+
+const [museuams, setMuseuams] = useState([])
+const [loading, setLoading] = useState(true)
+
+useEffect (()=> {
+  fetchMuseuams()
+},[])
+
+
+const fetchMuseuams = async ()=>{
+    
+   try{
+
+    const response = await axios.get(`https://fakestoreapi.com/products`)
+      console.log(response.data)
+      setMuseuams(response.data)
+      setLoading(false)
+   }catch(e){
+      console.log(e)
+   }
+    
+}
+
+  const muse = museuams.map(element => <li key ={element.id}>{element.title}</li>)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Spinner on Loading</h2>
+      {!loading ?( <ul>{muse}</ul> ):  < Spinner animation="border" /> }
     </div>
   );
 }
